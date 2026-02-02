512b01bf00cfd941ea9bd602c810d383f36a9818
yousucre <yousucre6@gmail.com>
2026-02-02 15:18:54 -0500

    Resuelto conflicto en package.json

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -5,13 +5,10 @@ const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret_key';
 
 export interface AuthRequest extends Request {
     user?: any;
-<<<<<<< HEAD
     params: any;
     body: any;
     query: any;
     headers: any;
-=======
->>>>>>> 3f64e6da120dd682c477398310e28aa34f10a743
 }
 
 export const authenticateToken = (req: AuthRequest, res: Response, next: NextFunction) => {
@@ -20,11 +17,7 @@ export const authenticateToken = (req: AuthRequest, res: Response, next: NextFun
 
     if (!token) return res.sendStatus(401);
 
-<<<<<<< HEAD
     jwt.verify(token, JWT_SECRET, (err: any, user: any) => {
-=======
-    jwt.verify(token, JWT_SECRET, (err, user) => {
->>>>>>> 3f64e6da120dd682c477398310e28aa34f10a743
         if (err) return res.sendStatus(403);
         req.user = user;
         next();
