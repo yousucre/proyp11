703940077f1ce9a998e750fdb1d33acb64a94ff4
JHON JAIRO CARRASCAL <jairoj1984@yahoo.es>
2026-02-06 22:05:04 -0500

    intentando coneccion frban

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBjajVBIv5SSEOas7GYEwVLAR2czS_nSFE",
+    authDomain: "proyp11.firebaseapp.com",
+    projectId: "proyp11",
+    storageBucket: "proyp11.firebasestorage.app",
+    messagingSenderId: "331528317563",
+    appId: "1:331528317563:web:656b5d892802d9317f90de"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
